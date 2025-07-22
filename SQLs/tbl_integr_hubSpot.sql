91407cbec535248ffd4bd1b88c10f768725fa24a
gittdr <112987600+gittdr@users.noreply.github.com>
2025-07-22 12:28:13 -0600

    Fixed logic to database

--- SQLs/tbl_integr_hubSpot.sql
+++ SQLs/tbl_integr_hubSpot.sql
@@ -1,27 +1,59 @@
-USE ControlPresupuestal
-CREATE TABLE tbl_integr_hubSpot(
-    id INT IDENTITY PRIMARY KEY,
-    Close_Date DATETIME,
-    Create_Date DATETIME,
-    Nombre NVARCHAR(255),
-    Deal_Stage NVARCHAR(80),
-    Deal_Type NVARCHAR(100),
-    Equipo_Colaborativo INT,
-    Monto_forecast DECIMAL(18, 2),
+/*
+El siguiente script tiene como objetivo poder generar la estructura de las tablas de la integracion con HubSpot
+*/
+USE AplicativosTDR
+GO
+
+DELETE [dbo].[tbl_integr_hubSpot_deals]
+GO
+-- DELETE [dbo].[tbl_integr_hubspot_pipelines]
+-- GO
+DELETE [dbo].[tbl_integr_hubspot_objects]
+GO
+
+CREATE TABLE [dbo].[tbl_integr_hubSpot_deals](
+	[Id] [nvarchar](50) PRIMARY KEY NOT NULL,
+	[Nombre] [nvarchar](255) NULL,
+	[Create_Date] [datetime] NULL,
+	[Close_Date] [datetime] NULL,
+	[Deal_Stage] [nvarchar](80) NULL,
+	[Pipeline] [nvarchar](80) NULL,
+	[Deal_Type] [nvarchar](100) NULL,
+	[Equipo_Colaborativo] [int] NULL,
+	[Monto_forecast] [decimal](18, 2) NULL,
+	[Is_Closed] [bit] NULL,
+	[Last_Modified_Date] [datetime] NULL,
+	[Industria] [nvarchar](100) NULL,
+	[Numero_de_Operadores] [int] NULL,
+	[Numero_de_Unidades] [int] NULL,
+	[Operacion] [nvarchar](50) NULL,
+	[Peso] [decimal](10, 2) NULL,
+	[Semirremolques] [int] NULL,
+	[Sucursal] [nvarchar](100) NULL,
+	[Tipo_de_Operador] [nvarchar](50) NULL,
+	[Tipo_de_Viaje] [nvarchar](50) NULL,
+	[Deal_Owner] [nvarchar](50) NULL,
+	[Deal_Owner_id] [bigint] NULL,
+	CONSTRAINT FK_DEALS_OBJECTS FOREIGN KEY (Id)
+		REFERENCES tbl_integr_hubspot_objects(Id)
+)
+GO
 
 
-    hs_lastmodifieddate DATETIME,
-    dealname NVARCHAR(255),
-    hs_is_closed_count BIT,
-    hs_object_id BIGINT,
-    industria NVARCHAR(100),
-    numero_de_operadores INT,
-    numero_de_unidades INT,
-    operacion NVARCHAR(50),
-    peso_ton DECIMAL(10, 2),
-    semirremolques INT,
-    sucursall NVARCHAR(100),
-    tipo_de_operador NVARCHAR(50),
-    tipo_de_viaje NVARCHAR(50),
-    unidades NVARCHAR(100)  -- Puede quedar NULL o como INT si sabes que es numérico
-);
+-- CREATE TABLE [dbo].[tbl_integr_hubspot_pipelines](
+-- 	[Id] [int] PRIMARY KEY,
+-- 	[Label] [nvarchar] (40),
+-- 	[Pipeline] [nvarchar] (100),
+-- 	[Pipeline_Id] [nvarchar] (40)
+-- )
+-- GO
+
+CREATE TABLE [dbo].[tbl_integr_hubspot_objects](
+	[Id] [nvarchar](50) PRIMARY KEY,
+	[emails] [int] NOT NULL,
+	[meetings] [int] NOT NULL,
+	[calls] [int] NOT NULL,
+	CONSTRAINT FK_OBJECTS_DEALS FOREIGN KEY(Id)
+		REFERENCES tbl_integr_hubSpot_deals(Id)
+)
+GO
--- main.py
+++ main.py
@@ -11,12 +11,13 @@ sys.stdout.reconfigure(encoding='utf-8')
 load_dotenv()
 
 url = os.getenv("API_URL") 
-endpoint = os.getenv("API_ENDPOINT")
 
 def get_deals_data(url_endpoint):
     paging = True
     page = ''
-    estatus = []
+    deals = []
+    deals_objects = []
+    pipelines_por_ejecutar = []
     """
     Fetch data from HubSpot API.
     :param api_key: Your HubSpot API key.
@@ -30,6 +31,10 @@ def get_deals_data(url_endpoint):
     url_owners = os.getenv("API_URL") + os.getenv("API_OWNERS_ENDPOINT") + os.getenv("API_OWNERS_PARAMS")
     owners = get_deal_owners(url_owners)
     try:
+        
+        pipeline_url = os.getenv("API_URL") + os.getenv("API_PIPELINES_ENDPOINT")
+        # get_deals_pipelines(pipeline_url)
+        pipelines = get_deals_pipelines(pipeline_url)
         
         while paging:
             url = f"{url_endpoint}{page}"
@@ -46,13 +51,21 @@ def get_deals_data(url_endpoint):
                     #         "propiedades" : result["properties"],
                     #     }
                     #     estatus["data"].append(data)
-                        
+                    
+                    for pipeline in pipelines:
+                        for stage in pipeline.get('data', []):
+                            if propierties.get("dealstage") == stage.get('id'):
+                                pipeline_id = pipeline.get('pipeline', 'Unknown Pipeline')
+                                stage_label = stage.get('label', 'Unknown Stage')
+                                break
+
                     data = {
-                        "Id_Objeto": propierties["hs_object_id"],
+                        "Id": propierties["hs_object_id"],
                         "Nombre": propierties["dealname"],
                         "Create_Date": safe_datetime(propierties.get("createdate")),
                         "Close_Date": safe_datetime(propierties.get("closedate")),
-                        "Deal_Stage": propierties.get("dealstage"),
+                        "Deal_Stage": stage_label,
+                        "Pipeline": pipeline_id,
                         "Deal_Type": propierties.get("dealtype"),
                         "Equipo_Colaborativo": safe_int(propierties.get("equipo_colaborativo")),
                         "Monto_forecast": safe_float(propierties.get("hs_forecast_amount")),
@@ -70,7 +83,16 @@ def get_deals_data(url_endpoint):
                         "Deal_owner": deal_owner,
                         "Deal_owner_id": safe_int(propierties.get("hubspot_owner_id"))
                     }
-                    estatus.append(data)
+                    deals.append(data)
+
+
+                    # Obtain deal objects
+                    if pipeline_id == 'Pipeline de ventas' and stage_label != 'Cierre perdido':
+                        deals_objects.append(propierties["hs_object_id"])
+                        # deal_objects = get_deals_objects(os.getenv("API_URL"), propierties["hs_object_id"])
+                        # deals_objects.append(deal_objects)
+                        # count += 1
+                        # print(count)
 
                 # Check if there is a next page
                 pagination = results.json().get('paging')
@@ -78,8 +100,11 @@ def get_deals_data(url_endpoint):
                     paging = False
                 else:
                     page = f'&after={pagination["next"]["after"]}'
-        save_to_file(estatus, 'estatus.json')
-        # return estatus
+        
+        save_to_file(deals, 'deals.json')
+        save_to_database(deals, os.getenv("QUERY_INSERT_DEAL"))
+        # save_to_file(deals_objects, 'deals_objects.json')
+        return deals_objects
     except Exception as e:
         print(f"Error fetching data: {e}")
         results.raise_for_status()  # Raise an error for bad responses
@@ -120,32 +145,92 @@ def get_deals_pipelines(url_endpoint):
         
         for pipeline in pipelines:
             stages = pipeline.get('stages', [])
+            pipeline_data = {
+                "pipeline": pipeline.get('label', 'Unknown'),
+                "data": []
+            }
             for stage in stages:
-                # data = {
-                #     stage.get('id', 'Unknown'): stage.get('label', 'Unknown')
-                # }
                 data = {
                     "id": stage.get('id', 'Unknown'),
-                    "label": stage.get('label', 'Unknown'),
-                    "id_pipeline": pipeline.get('id', 'Unknown'),
-                    "label_pipeline": pipeline.get('label', 'Unknown')
+                    "label": stage.get('label', 'Unknown')
                 }
-                return_data.append(data)
+                # data = {
+                #     "Id": stage.get('id', 'Unknown'),
+                #     "Label": stage.get('label', 'Unknown'),
+                #     "Pipeline": pipeline.get('label', 'Unknown'),
+                #     "Pipeline_Id": pipeline.get('id', 'Unknown')
+                # }
+                # pipeline_data[pipeline.get('label', 'Unknown')].extend(data)
+                pipeline_data["data"].append(data)
+            return_data.append(pipeline_data)
 
-        save_to_file(return_data, 'pipelines.json')
+
         # save_to_file(pipelines, 'pipelines.json')
-        return results.json()
+        # save_to_database(return_data, os.getenv("QUERY_INSERT_PIPELINE"))
+        
+        
+        # save_to_file(return_data, 'pipelines.json')
+        return return_data
     except Exception as e:
         print(f"Error fetching deal pipelines: {e}")
         return {}
 
-def save_deals_to_database(data):
+def get_deals_objects(url, deal_id):
+    """
+    Fetch deal objects from HubSpot API.
+    :return: JSON response containing deal objects.
+    """
+    
+    objects = ['emails', 'meetings', 'calls']
+    header = {
+        'Authorization': f'Bearer {os.getenv("API_TOKEN")}',
+    }
+    object_names = []
+    try:
+        for obj in objects:
+            paging = True
+            page = ''
+            count = 0
+            while paging:
+                url_endpoint = f"{url}/crm/v4/objects/deals/{deal_id}/associations/{obj}?limit=500{page}"
+                response = requests.get(url_endpoint, headers=header)
+                for object_result in response.json().get('results', []):
+                    count += 1
+                
+                # Check if there is a next page
+                pagination = response.json().get('paging')
+                if not pagination:
+                    paging = False
+                else:
+                    page = f'&after={pagination["next"]["after"]}'
+                    
+            data = {
+                obj : count
+            }
+            object_names.append(data)
+            
+        object_numbers = {
+            "deal_id": deal_id,
+            "emails": object_names[0].get('emails', 0),
+            "meetings": object_names[1].get('meetings', 0),
+            "calls": object_names[2].get('calls', 0)
+        }
+        
+        # save_to_file(object_numbers, 'objects.json')
+        # save_to_database(object_names, os.getenv("QUERY_INSERT_OBJECTS"))
+        
+        
+        
+        return object_numbers
+    except Exception as e:
+        print(f"Error fetching deal objects: {e}")
+        return {}
+
+def save_to_database(data, query):
     """
     Save the fetched data to a database.
     :param data: The data to save.
     """
-    url_owners = os.getenv("API_URL") + os.getenv("API_OWNERS_URL") + os.getenv("API_PARAMS2")
-    owners = get_deal_owners(url_owners)
     try:
         # Establecer conexión a SQL Server
         conn = pyodbc.connect(
@@ -159,58 +244,8 @@ def save_deals_to_database(data):
 
         # Insertar los registros
         for registro in data:
-            deal = registro["propiedades"]
-            owner_id = deal.get("hubspot_owner_id")
-            deal_owner = owners.get(owner_id, "Unknown Owner")
-            cursor.execute("""
-                INSERT INTO tbl_integr_hubSpot (
-                    Id_Objeto,
-                    Nombre,
-                    Create_Date,
-                    Close_Date,
-                    Deal_Stage,
-                    Deal_Type,
-                    Equipo_Colaborativo,
-                    Monto_forecast,
-                    Is_Closed,
-                    Last_Modified_Date,
-                    Industria,
-                    Numero_de_Operadores,
-                    Numero_de_Unidades,
-                    Operacion,
-                    Peso,
-                    Semirremolques,
-                    Sucursal,
-                    Tipo_de_Operador,
-                    Tipo_de_Viaje,
-                    Deal_owner,
-                    Deal_owner_id
-                )
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """, (
-                safe_datetime(deal.get("closedate")),
-                safe_datetime(deal.get("createdate")),
-                deal.get("dealname"),
-                safe_deal_stage(deal.get("dealstage")),
-                deal.get("dealtype"),
-                safe_int(deal.get("equipo_colaborativo")),
-                safe_float(deal.get("hs_forecast_amount")),
-                safe_int(deal.get("hs_is_closed_count")),
-                safe_datetime(deal.get("hs_lastmodifieddate")),
-                safe_int(deal.get("hs_object_id")),
-                deal.get("industria"),
-                safe_int(deal.get("numero_de_operadores")),
-                safe_int(deal.get("numero_de_unidades")),
-                deal.get("operacion"),
-                safe_float(deal.get("peso__ton_")),
-                safe_int(deal.get("semirremolques")),
-                deal.get("sucursall"),
-                deal.get("tipo_de_operador"),
-                deal.get("tipo_de_viaje"),
-                deal_owner,
-                safe_int(deal.get("hubspot_owner_id"))
-            ))
-            
+            data1 = registro.values()
+            cursor.execute(query, tuple(registro.values()))
         # Confirmar cambios
         conn.commit()
         print("Datos insertados correctamente en SQL Server.")
@@ -221,35 +256,6 @@ def save_deals_to_database(data):
     finally:
         # Cerrar conexión
         conn.close()
-
-def safe_deal_stage(deal_stage):
-    """
-    Safely convert a deal stage to a string, returning 'Unknown' if conversion fails.
-    :param deal_stage: The deal stage to convert.
-    :return: The converted deal stage or 'Unknown' if conversion fails.
-    """
-    deals = {
-        "3020163": "Lead",
-        "8594776" : "Por Arrancar",
-        "8594777" : "Cliente",
-        "qualifiedtobuy" : "Lead Autorizado",
-        "presentationscheduled" : "Negociacion",
-        "42501700" : "Lead No Autorizado",
-        "contractsent" : "Proceso de alta",
-        "42501702" : "Firma Cotización",
-        "decisionmakerboughtin" : "Contrato",
-        "42501701": "Tarifa",
-        "closedwon" : "Cierre comercial",
-        "appointmentscheduled" : "Formato de cotización",
-        "closedlost" : "Cierre perdido",
-        "39686207" : "Inactivo"
-    }
-    if deal_stage in deals:
-        return deals[deal_stage]
-    try:
-        return str(deal_stage)
-    except (ValueError, TypeError):
-        return 'Unknown'
 
 def safe_int(value):
     """
@@ -297,12 +303,26 @@ def save_to_file(data, filename):
     """
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
+    
+deals_url = os.getenv("API_URL") + os.getenv("API_DEALS_ENDPOINT") + os.getenv("API_DEALS_PARAMS")
+estatus = get_deals_data(deals_url)
+object_deals = []
+count = 0
+for deal in estatus:
+    print(deal + " - " + str(count))
+    count += 1
+    object_deals.append(get_deals_objects(url, deal))
+save_to_file(object_deals, 'deals_objects.json')
+save_to_database(object_deals, os.getenv("QUERY_INSERT_OBJECTS"))
+
+
+
 
 # pipeline_url = os.getenv("API_URL") + os.getenv("API_PIPELINES_ENDPOINT")
 # get_deals_pipelines(pipeline_url)
 
-deals_url = os.getenv("API_URL") + os.getenv("API_DEALS_ENDPOINT") + os.getenv("API_DEALS_PARAMS")
-estatus = get_deals_data(deals_url)
+
+# get_deals_objects(url, '39978390407')
 
 # url_endpoint = f"{url}{endpoint}{os.getenv('API_PARAMS1')}"
 # estatus = get_deals_data(url_endpoint)
